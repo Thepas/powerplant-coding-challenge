7f8b321eb6002503c9ac30efea413d1c5b2b9288
lepas <lepsmiland@gmail.com>
2021-06-20 22:19:04 +0200

    test update

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,9 @@
-import requests as requests
+import requests
 
 url = 'http://127.0.0.1:8888/productionplan'
 
 if __name__ == "__main__":
-    payload_file = "../example_payloads/payload4.json"
+    payload_file = "example_payloads/payload4.json"
 
     payload = open(payload_file, 'rb').read()
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
