b15dffeb24ba62edb7d2255ad64a4d48fc499ddf
Thepas <45799484+Thepas@users.noreply.github.com>
2021-06-20 22:25:39 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ import requests
 url = 'http://127.0.0.1:8888/productionplan'
 
 if __name__ == "__main__":
-    payload_file = "example_payloads/payload4.json"
+    payload_file = "example_payloads/payload1.json"
 
     payload = open(payload_file, 'rb').read()
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
